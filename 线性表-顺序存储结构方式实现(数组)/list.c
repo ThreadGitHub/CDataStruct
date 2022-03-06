3d3c8064347e31761e9951563b3d4a2e5c399141
初广鹏 <ThreadEmail@163.com>
2022-03-06 21:08:19 +0800

    线性表-顺序存储结构实现(数组)

---
+++ 线性表-顺序存储结构方式实现(数组)/list.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "list.h"
+
+Status getElem(List list, int i, ElemType *e){
+    if(list.length == 0 || i < 1 || i > list.length){
+        return Error;
+    }
+    *e = list.data[i - 1];
+    return OK;
+}
+
+Status ListInsert(List *list, int i, ElemType e){
+    if(list->length == maxLength){
+        return Error;
+    }
+    //这里是可以允许的范围是 1 - 目前所在数量 +1  这个+1就是允许在队尾追加
+    if(i < 1 || i > list->length + 1){
+        return Error;
+    }
+    if(i <= list->length){
+        for(int k = list->length-1;k >= i-1;k--){
+            list->data[k+1] = list->data[k];
+        }
+    }
+    list->data[i-1] = e;
+    list->length+=1;
+    return OK;
+}
+
+Status ListDelete(List *list, int i, ElemType *e){
+    if(i == 0){
+        return Error;
+    }
+    if(i < 0 || i > list->length){
+        return Error;
+    }
+    i -= 1;
+    *e = list->data[i];
+    if(i != list->length){
+        for(int num = i+1;num < sizeof (list->data) / sizeof (list->data[0]);num++){
+            list->data[num-1] = list->data[num];
+        }
+    }
+    list->length--;
+    return OK;
+}
+
+void testList() {
+    int num = 0;
+    ElemType *el = &num;
+    List list;
+    list.length = 0;
+    int length = sizeof(list.data) / sizeof(list.data[0]);
+    for(int i = 0;i < length;i++){
+        list.data[i] = 0;
+    }
+
+    List *listPoint = &list;
+    ListInsert(listPoint, 1, 1);
+    ListInsert(listPoint, 2, 2);
+    ListInsert(listPoint, 3, 3);
+    ListInsert(listPoint, 4, 4);
+    ListInsert(listPoint, 2, 996);
+    ListInsert(listPoint, 10, 777);
+    Status  result = ListInsert(listPoint, 11, 777);
+
+    printf("顺序表 : ");
+    for (int i = 0; i < length; ++i){
+        printf("%d\t", list.data[i]);
+    }
+
+    ElemType elemType = 0;
+    ElemType *elemType_p = &elemType;
+    getElem(list, 4, elemType_p);
+    printf("\n第4个元素 : %d", *elemType_p);
+    getElem(list, 2, elemType_p);
+    printf("\n第2个元素 : %d", *elemType_p);
+    getElem(list, 10, elemType_p);
+    printf("\n第10个元素 : %d", *elemType_p);
+
+    //删除第一个元素
+    ListDelete(listPoint, 1,elemType_p);
+    //删除第三个元素
+    ListDelete(listPoint, 3,elemType_p);
+}
---
+++ 线性表-顺序存储结构方式实现(数组)/list.h
@@ -0,0 +1,52 @@
+//
+// Created by Thread on 2022/2/12.
+//
+
+#ifndef CDATASTRUCT_LIST_H
+#define CDATASTRUCT_LIST_H
+
+#define maxLength 10
+#define OK 1
+#define Error 0
+#define True 1
+#define False 0
+typedef int ElemType;
+typedef int Status;
+
+typedef struct List {
+    ElemType data[maxLength];
+    int length;
+} List;
+
+/**
+ * 获取线性表中指定元素的值
+ * @param list 线性表
+ * @param i 获取元素的索引
+ * @param e 元素的返回值
+ * @return
+ */
+Status getElem(List list, int i, ElemType *e);
+
+/**
+ * 线性表 插入操作
+ * @param list 线性表
+ * @param i 插入元素的位置
+ * @param e 接收值
+ * @return
+ */
+Status ListInsert(List *list, int i, ElemType e);
+
+/**
+ * 线性表 删除操作
+ * @param list 线性表
+ * @param i 删除元素位置
+ * @param e 接收值
+ * @return
+ */
+Status ListDelete(List *list, int i, ElemType *e);
+
+/**
+ * 测试函数
+ */
+void testList();
+#endif //CDATASTRUCT_LIST_H
